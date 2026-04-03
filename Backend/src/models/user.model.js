b3a3565cf6ba91b5294a9175d56ca4a1fcac193e
Hassan <malik.hr.2004@gmail.com>
2026-04-03 16:54:00 +0500

    Add some utilities in my backend

---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,62 @@
+import mongoose, {Schema} from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jwt";
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: [true, "email is required"],
+    unique: true
+  },
+  password: {
+    type: String,
+    required: [true, "password is required"],
+    unique: true
+  },
+  confirmPassword: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  role: {
+    type: String,
+    enum: ["Student", "Author", "Admin"],
+    required: true
+  }
+},
+{timestamps: true})
+
+// check password is correct or not
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+}
+
+// generate access token
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      password: this.password
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: ACCESS_TOKEN_EXPIRY
+    }
+  )
+}
+
+// generate refresh token
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: REFRESH_TOKEN_EXPIRY
+    }
+  )
+}
+
+export const User = mongoose.model("User", userSchema)
