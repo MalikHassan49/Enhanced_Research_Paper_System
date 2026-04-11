2875b7602ee7d75198cf26829334d67b5c3887d6
Hassan <malik.hr.2004@gmail.com>
2026-04-11 07:45:44 +0500

    Enhance author dashboard and submit paper functionality: update CSS for layout adjustments, add footer, and implement file upload feature with JavaScript event handling.

--- Frontend/script.js
+++ Frontend/script.js
@@ -163,3 +163,18 @@ if(loginForm) {
     }
   })
 }
+
+// submitPaper.html (Files temporary store in browser code)
+
+const fileUpload = document.getElementById("fileUpload");
+const fileText = document.getElementById("fileText");
+
+fileUpload.addEventListener("change", () => {
+  console.log("File data: ", fileUpload.files);
+  console.log("First File data: ", fileUpload.files[0]);
+
+  if(fileUpload.files.length > 0) {
+    fileText.textContent = fileUpload.files[0].name;
+  }
+  
+});
